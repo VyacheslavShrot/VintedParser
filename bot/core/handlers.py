7fa9de4bdb343a4b6bf06a06bc4c63af2f2e30fe
crm.c.31904 <crm.c.31904@dz.com>
2024-10-17 19:03:30 +0200

    Formatting Code

--- bot/core/handlers.py
+++ bot/core/handlers.py
@@ -68,10 +68,8 @@ class Handlers:
 
             start_url = message.text
 
-            logger.info("00000")
             browser.get(start_url)
 
-            logger.info("1111")
             # WebDriverWait(browser, 25).until(
             #     EC.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler'))
             # )
@@ -81,7 +79,6 @@ class Handlers:
             # if button_cookie.is_displayed() and button_cookie.is_enabled():
             #     button_cookie.click()
 
-            logger.info("After Click Cookie Button")
             existed_boxes: list = []
 
             while True:
@@ -90,23 +87,19 @@ class Handlers:
                 )
 
                 item_boxes = browser.find_elements(By.CLASS_NAME, 'new-item-box__container')
-                logger.info("1")
 
                 try:
                     for box in item_boxes[:7]:
                         unique_id = box.get_attribute('data-testid')
-                        logger.info("2")
 
                         if unique_id not in existed_boxes:
                             existed_boxes.append(unique_id)
-                            logger.info("3")
 
                             """
                             Get URL of Item
                             """
                             link = box.find_element(By.CSS_SELECTOR, '.new-item-box__overlay.new-item-box__overlay--clickable')
                             href = link.get_attribute('href')
-                            logger.info("4")
 
                             """
                             Get Size of Item
@@ -114,7 +107,6 @@ class Handlers:
                             size_boxes = box.find_elements(By.CLASS_NAME, 'new-item-box__description')
                             size_element = size_boxes[1].find_element(By.CSS_SELECTOR, '.web_ui__Text__text.web_ui__Text__caption.web_ui__Text__left')
                             size = size_element.text
-                            logger.info("5")
 
                             """
                             Get Images of Item
@@ -122,7 +114,6 @@ class Handlers:
                             images = box.find_elements(By.CLASS_NAME, 'web_ui__Image__content')
                             src_list = [img.get_attribute('src') for img in images if img.get_attribute('src')]
                             src_list.pop(0)
-                            logger.info("6")
 
                             """
                             Get Price of Item
@@ -130,33 +121,26 @@ class Handlers:
                             price = box.find_element(
                                 By.CSS_SELECTOR, '.web_ui__Text__text.web_ui__Text__caption.web_ui__Text__left.web_ui__Text__muted'
                             )
-                            logger.info("7")
 
                             if href and images and price.text and size:
                                 response = f"{price.text}\n{size}\n\n{href}"
-                                logger.info("8")
 
                                 if len(src_list) == 1:
                                     await message.answer_photo(src_list[0], caption=response)
-                                    logger.info("9")
                                 elif len(src_list) > 1:
                                     media = [InputMediaPhoto(media=src) for src in src_list]
                                     media[0].caption = response
                                     await message.answer_media_group(media)
-                                    logger.info("10")
 
                                 await asyncio.sleep(1)
                 except Exception as e:
                     logger.error(f"Error occurred while iterate in boxes | {e}")
                     continue
 
-                logger.info("11")
-
                 await asyncio.sleep(3)
 
                 browser.refresh()
 
-                logger.info("12")
         except Exception as e:
             logger.error(
                 f"An unexpected error in Task | {e}"
